eef93946920ccfbcc6e4606e3a306375fdb6ad13
thihuli <thiagocandidof@hotmail.com>
2019-05-11 22:22:48 -0300

    criando componente conversor

---
+++ conversor-moeda/src/components/Conversor.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+export default class Conversot extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>
+          <p>esse é um conversor</p>
+      </div>
+    );
+  }
+}
