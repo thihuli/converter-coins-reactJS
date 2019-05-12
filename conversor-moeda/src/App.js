8376c4ec464330b569718c0e05ab5ed8ec87bab9
thihuli <thiagocandidof@hotmail.com>
2019-05-11 22:01:25 -0300

    app conversor de moedas em reactJS

---
+++ conversor-moeda/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import Conversor from "./components/Conversor";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Conversor moedaA ="USD" moedaB="BRL"></Conversor>
+    </div>
+  );
+}
+
+export default App;
