af9c16245c15180c02b9728c25e5435ee4d2a504
thihuli <thiagocandidof@hotmail.com>
2019-05-12 12:15:25 -0300

    criando função converter

--- conversor-moeda/src/App.js
+++ conversor-moeda/src/App.js
@@ -7,7 +7,7 @@ import Conversor from "./components/Conversor";
 function App() {
   return (
     <div className="App">
-      <Conversor moedaA ="USD" moedaB="BRL"></Conversor>
+      <Conversor moedaA ="CAD" moedaB="BRL"></Conversor>
     </div>
   );
 }
--- conversor-moeda/src/components/Conversor.js
+++ conversor-moeda/src/components/Conversor.js
@@ -5,13 +5,30 @@ export default class Conversot extends Component {
   constructor(props) {
     super(props);
     this.state = {
+        moedaA_valor:"",
+        moedaB_valor:0,
     };
+  
+    this.converter = this.converter.bind(this);
+    
   }
+
+    
+
+  converter(){
+
+    console.log(this.state);
+
+
+  };
 
   render() {
     return (
       <div>
-          <p>esse é um conversor</p>
+          <h2>{this.props.moedaA}para {this.props.moedaB}</h2>
+          <input type="text" onChange={(event) => {this.setState({moedaA_valor:event.target.value})}}></input>
+          <input type="button" value="converter" onClick={this.converter}></input>
+          <h2>valor convertido</h2>
       </div>
     );
   }
